fc2e3dbb05d559dbd868889f99000f7807eb0b34
Rifky Hilman <rifkyhilmanudin965@gmail.com>
2024-11-20 19:48:44 +0700

    add detail product

--- src/views/products/Detail.vue
+++ src/views/products/Detail.vue
@@ -1,6 +1,19 @@
 <template>
     <div>
-        <h1>Detail Product Page</h1>
+      <div id="page-wrap">
+        <div class="img">
+          <img src="" alt="">
+        </div>
+        <div id="product-details">
+            <h1>Dark Wallet</h1>
+            <h3 id="price">Rp.123.000</h3>
+            <p>Average rating : 5.0</p>
+            <button id="add-to-cart">Add to Cart</button>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem omnis quos nam repudiandae neque provident debitis veritatis, reprehenderit doloremque culpa. Reiciendis illo illum nostrum nobis tenetur sequi tempora amet neque.
+            </p>
+        </div>
+      </div>
     </div>
 </template>
 
